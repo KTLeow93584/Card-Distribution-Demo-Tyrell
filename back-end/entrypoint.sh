baf08037f7030ccf1abac0ba078c24198557dcfa
Leow Kean Tat (Kaz) <leowkeantat@gmail.com>
2025-07-06 15:09:34 +0800

    Applied fixes to docker-related files, removed the exposed app key from the .env docker file, and added comment in docker-compose regarding limitations with mounting and the ".env" file.

--- back-end/entrypoint.sh
+++ back-end/entrypoint.sh
@@ -7,5 +7,8 @@ php artisan migrate --force
 # Optional: install dependencies (safe inside container)
 # composer install --no-interaction --prefer-dist --optimize-autoloader
 
+# Generate the App Key
+php artisan key:generate
+
 # Start Laravel server
 php artisan serve --host=0.0.0.0 --port=8000
